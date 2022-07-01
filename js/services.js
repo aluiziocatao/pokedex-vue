47e7bf5b292a04231831120d666bcd94539b93b0
aluiziocatao <aluizio2210@gmail.com>
2022-07-01 13:05:50 -0500

    Alteracoes

--- js/services.js
+++ js/services.js
@@ -2,14 +2,15 @@ var PokeService = {
   API: {
     url: '//pokeapi.co/api/v1/',
     get: (url) => {
-      return fetch(`PokeService.API.url${url}`).then(response =>
-        response.json()
+      return fetch(`PokeService.API.url${url}`)
+        .then(response => response.json()
       )
     },
     Pokemon: {
       listAll: () => {
-        return PokeService.API.get('pokedex/1').then(response => {
-          return response.pokemon
+        return PokeService.API.get('pokedex/1')
+          .then(response => {
+            return response.pokemon
             .map(pokemon => {
               pokemon.number = PokeService.getNumberFromURL(
                 pokemon.resource.uri
@@ -20,7 +21,7 @@ var PokeService = {
             .sort((a, b) => {
               return a.number > b.number ? 1 : -1
             })
-        })
+          })
       }
     }
   },
